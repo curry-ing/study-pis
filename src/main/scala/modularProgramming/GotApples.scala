d1ab7f19be7cf37c19e55c1fe63ccedc4dd91c9e
Ma Sunghoon <masunghoon@gmail.com>
2017-08-25 05:14:06 +0900

    restore gotApples

--- src/main/scala/modularProgramming/GotApples.scala
+++ src/main/scala/modularProgramming/GotApples.scala
@@ -9,7 +9,7 @@ object GotApples {
         SimpleDatabase
 
     object browser extends Browser {
-      val database: db.type = db
+      val database = db
     }
 
     val apple = SimpleDatabase.foodNamed("Apple").get
