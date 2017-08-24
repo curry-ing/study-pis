7efbf5417e9f66f09d58eec00b63f176fb107a8c
Ma Sunghoon <masunghoon@gmail.com>
2017-08-25 05:16:44 +0900

    10.1 Create same object with GotApples

---
+++ src/main/scala/modularProgramming/GotApples2.scala
@@ -0,0 +1,20 @@
+package modularProgramming
+
+object GotApples2 {
+  def main(args: Array[String]) = {
+    val db: Database =
+      if (args(0) == "student")
+        StudentDatabase
+      else
+        SimpleDatabase
+
+    object browser extends Browser {
+      val database = db
+    }
+
+    val apple = SimpleDatabase.foodNamed("Apple").get
+
+    for (recipe <- browser.recipesUsing(apple))
+      println(recipe)
+  }
+}
